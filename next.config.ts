eab548ebe4c705aff64b4f6fdb7b2d255e3606a2
romanlp3005 <romanlayani@gmail.com>
2026-03-01 16:12:29 +0100

    Add basePath to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Indique à Next.js de générer des fichiers statiques
+  output: "export",
   images: {
-    unoptimized: true, // Requis si vous utilisez le composant <Image /> de Next.js
+    unoptimized: true,
   },
+  basePath: "/portfolio2", // Indique à Next.js qu'il est dans un sous-dossier sur GitHub
 };
 
 export default nextConfig;
